33fa05db86d4ac7c879149e5054eb44889da4232
jasonslepicka <jasonslepicka@gmail.com>
2016-10-05 14:20:14 -0700

    Update to dig extractor 0.2.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 
 setup(
     name = 'digAddressExtractor',
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'digAddressExtractor',
     author = 'Lingzhe Teng',
     author_email = 'zwein27@gmail.com',
--- test/test_address_extractor.py
+++ test/test_address_extractor.py
@@ -24,7 +24,7 @@ class TestAddressExtractorMethods(unittest.TestCase):
         extractor = AddressExtractor().set_metadata({'extractor': 'address'})
         extractor_processor = ExtractorProcessor().set_input_fields(['content']).set_output_field('extracted').set_extractor(extractor)
         updated_doc = extractor_processor.extract(doc)
-        self.assertEqual(updated_doc['extracted']['value'], {'input': "Very passable black 25 year young TS girl with the best of the best! 9193959158 hosting off Western Boulevard NCstate area I'm waiting! 20-40 $pecial$", 'address': ['hosting off western boulevard']})
+        self.assertEqual(updated_doc['extracted'][0]['value'], {'input': "Very passable black 25 year young TS girl with the best of the best! 9193959158 hosting off Western Boulevard NCstate area I'm waiting! 20-40 $pecial$", 'address': ['hosting off western boulevard']})
 
     
 
